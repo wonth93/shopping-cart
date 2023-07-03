8e84382ceccf7cd549791260a1e312466d040e6f
wonth93 <wongth93@gmail.com>
2023-07-03 14:13:03 -0700

    add click event

---
+++ index.js
@@ -0,0 +1,7 @@
+const input = document.getElementById("input-field");
+const button = document.getElementById("add-button");
+
+button.addEventListener("click", () =>{
+  const inputValue = input.value;
+  console.log(inputValue);
+})
