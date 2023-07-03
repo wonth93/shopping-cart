ffd531d02779ca64469f57f0000f4f7210e59f8c
wonth93 <wongth93@gmail.com>
2023-07-03 14:45:03 -0700

    connect to firebase db

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+
+const db = {
+  databaseURL: "https://shopping-cart-e321d-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(db);
+
+console.log(app)
+
 const input = document.getElementById("input-field");
 const button = document.getElementById("add-button");
 
