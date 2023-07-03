fceb9a4779faa5b8e900595f87ebb610ba49e026
wonth93 <wongth93@gmail.com>
2023-07-03 15:14:03 -0700

    push data to database

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import { getDatabase, ref, push } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
-const db = {
+// location of the database
+const appSettings = {
   databaseURL: "https://shopping-cart-e321d-default-rtdb.firebaseio.com/"
 }
 
-const app = initializeApp(db);
-
-console.log(app)
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const data = ref(database, "EPL Team")
 
 const input = document.getElementById("input-field");
 const button = document.getElementById("add-button");
 
 button.addEventListener("click", () =>{
   const inputValue = input.value;
-  console.log(inputValue);
+
+  // Push data to the database
+  push(data, inputValue)
+
+  console.log(`${inputValue} is added to databse...`);
 })
